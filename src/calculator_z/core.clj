df2ec15846f7939f31bb16747b7cb2e2f711ab2f
jpmendesdev <jotamendes944@gmail.com>
2025-05-20 14:54:03 -0300

    jvm-opts em project.clj para resolver alguns erros da jvm e criação do método busca-calorias que da um get com uma label descricao lendo o nome do alimento

--- src/calculator_z/core.clj
+++ src/calculator_z/core.clj
@@ -1,35 +1,44 @@
 (ns calculator-z.core
+  (:require [clj-http.client :as client])
   (:gen-class))
 
-;;(def chave "")
 ;; gravar as transações no atom
 ;; usar o swap!
 
-(def api-url "https://caloriasporalimentoapi.herokuapp.com/")
+(def api-url "https://caloriasporalimentoapi.herokuapp.com/api/calorias/")
 
-(http-client/get api-url
-  {:query-params {"q" "calorias"
-                  "api-Key" chave}})
+ 
+
+(defn buscar-calorias [alimento]
+  (let [res (client/get api-url
+                        {:query-params {"descricao" alimento}
+                        :as :json})]
+    (:body res)))
+
+(defn consulta-API []
+   (let [entrada (read-line)
+      resultado (buscar-calorias entrada)]
+    (run!
+      #(println (str "\"" (:descricao %) "\" contem " (:calorias %) " calorias por " (:quantidade %)))
+      resultado)))
 
 
-
-(defn ganho []
-  (println "Chamando funcao ganho"))
+;; (defn ganho []
+;;   (println "Chamando funcao ganho"))
 
 (defn perda []
   (println "Chamando funcao perda"))
 
 
-
 (defn -main []
   (println "\nMenu")
-  (println "Opcao 1 - Ganho")
+  (println "Opcao 1 - Consulta API")
   (println "Opcao 2 - Perda")
   (println "Opcao 3 - Sair")
   (def op (read))
 
   (cond
-    (= op 1) (do (ganho) (recur))
+    (= op 1) (do (consulta-API) (recur))
     (= op 2) (do (perda) (recur))
     (= op 3) (println "Saindo...")))
 
